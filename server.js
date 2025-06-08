ab9bd6c4986f18a8d2ce00b521b262e20fcce040
saida9402 <saidahaymatova@gmail.com>
2025-06-08 13:51:34 +0900

    feat: refactoring

--- server.js
+++ server.js
@@ -41,11 +41,14 @@ app.get("/author", (req, res) => {
 
 app.get("/", function (req, res) {
   ///data basedan malumot olib o'qish uchun <get> ishlatiladi
-  res.render("harid");
+  res.render("reja");
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-  console.log(`The server is running successfully on port: ${PORT}`);
+  console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+
+  );
 });
+
