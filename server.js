f65e069e7d2070925f81506263c055b9f33ecbe8
saida9402 <saidahaymatova@gmail.com>
2025-06-10 11:41:14 +0900

    feat: mongodb ulanish logic

--- server.js
+++ server.js
@@ -1,54 +1,41 @@
-console.log("web serverni boshlash");
-const express = require("express");
-const app = express();
+
 const http = require("http");
-const fs = require("fs");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
-  }
-});
+const mongodb = require ("mongodb");
 
-// 1. KIrish code // expressga kirib kevotgan ma'lumotlarga bog'liq bo'gan kodlar yoziladi.
-app.use(express.static("public")); //papkasi ichidagi fayllar (CSS, rasmlar, JS fayllar) front-endda to‘g‘ridan-to‘g‘ri ko‘rsatiladi.
-app.use(express.json()); //kirib keladigon json datani object holatiga o'girib beradi. (ya'ni klaynt va web server data json ko'rinishida bo'ladi)
-app.use(express.urlencoded({ extended: true })); ///htmldan form request degan traditional request qilish instrumenti bor. formdan biror narsa yzoilsa express server qabul qiladi. yozilmasa serverga kiritmag ignore qiladi.
+let db;
+const connectionString = "mongodb+srv://deena94:dO9F4rltdHVq0Eda@cluster0.qdmubkf.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster0";
 
-//2. Session code
+mongodb.connect(
+    connectionString,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err, client) => {
+        if (err) console.log("ERROR on connction MongoDB");
+        else{
+            console.log("MongoDB connection succeed");
+            module.exports = client;
 
-//3. Views code
-app.set("views", "views"); //views folder ichidan o'qidi. ejs orqali html frontendi backend ichidan yasash-> views — EJS fayllar joylashgan katalog nomi (views/harid.ejs).
-app.set("view engine", "ejs"); //BSSR backendda htmlni qurib browserga yuborish  ejs — bu templating engine bo‘lib, HTML ichida JS kod yozish imkonini beradi.
+            const app = require("./app");
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function(){
+                console.log(
+                    `The server is runnning successfully on port: ${PORT}, http://localhost:${PORT}`);
+            });
+        }
+    }
+);
 
-//req.body — foydalanuvchidan kelgan ma’lumot.
-//res.json() — mijozga JSON formatda javob qaytaradi.
-//➡ Bosh sahifaga (/) kirilsa, views/harid.ejs fayli render qilinadi.
+// module.exports = client;
 
-//4. Routing code - address shakllantirish
-app.post("/create-item", (req, res) => {
-  ///ma'lum bir ma'lumotni o'zi bilan olib keladi va data basega shuni yozadi
-  // console.log(req.body);
-  // res.json({ test: "success" });
-});
 
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
+// const server = http.createServer(app);
+// let PORT = 3000;
+// server.listen(PORT, function () {
+//   console.log(`The server is running successfully on port: ${PORT}, `
 
-app.get("/", function (req, res) {
-  ///data basedan malumot olib o'qish uchun <get> ishlatiladi
-  res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
-
-  );
-});
-
+//   );
+// });
